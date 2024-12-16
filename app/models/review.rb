7fb14b85d804c67a2364a67a0d7f2bb645ff97d0
shibamon48 <eaglel4rk@gmail.com>
2024-12-16 20:51:23 +0900

    バリデーションエラー時のレイアウト崩れ対応

--- app/models/review.rb
+++ app/models/review.rb
@@ -8,4 +8,5 @@ class Review < ApplicationRecord
 
   validates :name, presence: true
   validates :maker, presence: true
+  validates :rating, comparison: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
 end
